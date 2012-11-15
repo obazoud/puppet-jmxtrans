83e4e489de1cadec0945c25e2eddf4394b997e9d
Andrew Otto <acotto@gmail.com>
2012-11-15 16:15:31 -0500

    Added documentation for jmxtrans::metrics

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,23 @@ class jmxtrans {
 
 # == Class jmxtrans::metrics
 #
-# TODO:  Think of a better name for this
+# Writes a jmxtrans JSON file in
+# /var/lib/jmxtrans/$title.json.
+# Each file in this directory should correspond to a single
+# JMX instance and specify the object and metric names to query
+# the JMX instance for.
+#
+# == Parameters
+# $jmx                  - host:port of JMX to query (required)
+# $queries              - array of hashes of the form [ { "obj" => "JMX object name", "attr" => [ "array", "of", "JMX", "metric", "names" ] }, ... ]
+# $jmx_username         - JMX username (if there is one)
+# $jmx_password         - JMX password (if there is one)
+# $resultAlias
+# $ganglia              - host:port of Ganglia gmond
+# $ganglia_group_name   = Ganglia metrics group
+# $graphite             = host:port of Graphite server
+# $graphite_root_prefix = 
+# $outfile              = local file path in which to save metric query results.
 define jmxtrans::metrics(
 	$jmx,
 	$queries,
